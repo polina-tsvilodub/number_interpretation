3d7bb95e0477f3cd598e94fd73d80d6a8e533e0e
polina-tsvilodub <polina.tsvilodub@gmail.com>
2025-01-16 11:00:13 +0100

    add prior only one shot CoT prompt

--- code/src/evaluate_llm_hyperbole.py
+++ code/src/evaluate_llm_hyperbole.py
@@ -97,7 +97,7 @@ elif args.prompt == "1shot_cot":
         with open(os.path.join("../prompt_instructions/advanced_prompting", "evaluation_1shot_1a_cot_goal.txt"), 'r') as f:
             prompt = f.read().strip()
     elif args.expt_num == "1b":
-        with open(os.path.join("../prompt_instructions/advanced_prompting", "evaluation_1shot_1b_cot_goal.txt"), 'r') as f:
+        with open(os.path.join("../prompt_instructions/advanced_prompting", "evaluation_1shot_1b_cot_prior.txt"), 'r') as f:
             prompt = f.read().strip()
 else:
     raise ValueError(f"Prompt {args.prompt} not found.")
@@ -185,5 +185,5 @@ for j in range(args.num):
     if not os.path.exists(os.path.join(args.output_dir, datafile)):
         os.makedirs(os.path.join(args.output_dir, datafile))
 
-    prefix = f"{args.model.replace('/','_')}_{args.prompt}_{args.temperature}_{args.num}_{args.offset}_iter{j}"
+    prefix = f"{args.model.replace('/','_')}_{args.prompt}Prior_{args.temperature}_{args.num}_{args.offset}_iter{j}"
     data_out.to_csv(os.path.join(args.output_dir, datafile, f"{prefix}_{args.expt_num}_predicted_answers.csv"), index=False)
--- code/src/run_slurm_job.sh
+++ code/src/run_slurm_job.sh
@@ -38,7 +38,7 @@ echo " "
 module load devel/cuda/11.6
 
 # iterate over tasks
-tasks=("1a" "1b") # "1b" "2" "3a" "3b")
+tasks=("1b") # "1b" "2" "3a" "3b")
 # iterate over models
 models=("gpt-4o-mini") # "claude-3-5-sonnet-20241022" "google/gemma-1.1-7b-it" "allenai/OLMo-2-1124-13B-Instruct" "gemini-1.5-pro" "meta-llama/Llama-3.3-70B-Instruct")
 
